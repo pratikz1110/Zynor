e28fa72bdad27ded131dc0b0ceee719c5ac4fe9e
pratikz1110 <pratik.zendge@gmail.com>
2025-12-17 12:09:10 -0800

    update gitignore and terraform outputs

--- infra/terraform/staging/outputs.tf
+++ infra/terraform/staging/outputs.tf
@@ -3,3 +3,15 @@ output "ecr_repository_url" {
   description = "ECR repository URL for zynor-api"
 }
 
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.staging.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.zynor_api.name
+}
+
+output "api_security_group_id" {
+  value = aws_security_group.zynor_api.id
+}
+
